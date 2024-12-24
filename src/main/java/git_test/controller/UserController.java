72997e535dcd11ff29264d04f72f694e96fde0d9
laxminideva59 <laxminideva593@gmail.com>
2024-12-24 17:15:16 +0530

    1. Created a Rest Controller UserController to handle /user requests 2. created the first method to handle /user/greet/request.

---
+++ src/main/java/git_test/controller/UserController.java
@@ -0,0 +1,17 @@
+package git_test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    
+	@GetMapping("/greet")
+	public String greet() {
+		return"Welcome to git!";
+	}
+	
+	
+}
